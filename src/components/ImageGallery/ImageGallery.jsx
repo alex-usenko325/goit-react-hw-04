5022907ad6a2b054681781859d57309d441fe02d
Zergus325 <alex.usenko.97@gmail.com>
2024-11-27 00:57:57 +0200

    gallery and card done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -4,15 +4,15 @@ import s from './ImageGallery.module.css';
 const ImageGallery = ({ images, openModal }) => {
   return (
     <ul className={s.gallery}>
-      {images.map((image) => (
-        <li key={image.id} className={s.galleryItem}>
-          <ImageCard
-            src={image.urls.small}
-            alt={image.alt_description}
-            onClick={() => openModal(image)}
-          />
-        </li>
-      ))}
+      {images.map(({ id, urls, alt_description }) => (
+  <li key={id} className={s.galleryItem}>
+    <ImageCard
+      src={urls.small}
+      alt={alt_description}
+      onClick={() => openModal(images)}
+    />
+  </li>
+))}
     </ul>
   );
 };
